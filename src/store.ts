7eb58c89011a2e9ba0ec6fab2df13610a1a79813
mklinovsky <michal.klinovsky@gmail.com>
2025-02-09 09:33:33 +0100

    Add conversations to store

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,12 @@
 import { JSONFilePreset } from 'lowdb/node';
+import { type AiMessage, type Conversation } from './types.ts';
+import { randomUUID } from 'node:crypto';
 
 type Data = {
-  messages: string[];
+  conversations: Conversation[];
 };
 
-const defaultData: Data = { messages: [] };
+const defaultData: Data = { conversations: [] };
 
 async function getDb() {
   const db = await JSONFilePreset<Data>('db.json', defaultData);
@@ -15,3 +17,37 @@ export async function getData() {
   const db = await getDb();
   return db.data;
 }
+
+export async function createConversation() {
+  const db = await getDb();
+  const conversation = { id: randomUUID(), messages: [] };
+
+  db.data.conversations.push(conversation);
+  await db.write();
+
+  return conversation.id;
+}
+
+export async function saveMessages(
+  conversationId: string,
+  messages: AiMessage[],
+) {
+  const db = await getDb();
+  const conversation = db.data.conversations.find(
+    (c) => c.id === conversationId,
+  );
+
+  if (!conversation) {
+    throw new Error(`Conversation with id ${conversationId} not found`);
+  }
+
+  conversation.messages.push(...messages);
+  await db.write();
+}
+
+export async function clearData() {
+  const db = await getDb();
+  db.data = defaultData;
+  await db.write();
+}
+
